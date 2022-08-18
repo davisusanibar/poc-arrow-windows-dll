2c7d28228eb6101eb5551563c974b966ffc6bd3e
david dali susanibar arce <davi.sarces@gmail.com>
2022-08-18 18:54:51 -0500

    External ci build sh

--- src/ci/demo.sh
+++ src/ci/demo.sh
@@ -2,6 +2,7 @@
 
 set -ex
 if [[ "$(uname -s)" == "Linux" ]] && [[ "$(uname -m)" == "s390x" ]]; then
+  ls
 fi
 
 mvn="mvn -B -DskipTests -D'rat.skip'=true -D'org.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener'=warn -T 2C"
